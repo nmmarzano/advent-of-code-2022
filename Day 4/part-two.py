e24a6354f2d58c040542ef6c878894fea15020d7
Stella <nmmarzano@gmail.com>
2022-12-04 11:25:18 -0300

    complete day 4

---
+++ Day 4/part-two.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def is_either_contained(range_1, range_2):
+    first_l, first_r = map(lambda a: int(a), range_1.split('-'))
+    second_l, second_r = map(lambda a: int(a), range_2.split('-'))
+    
+    return first_l <= second_r and first_r >= second_l
+
+
+if __name__ == '__main__':
+    count = 0
+
+    with open(sys.argv[1]) as file:
+        for line in file:
+            if is_either_contained(*line.strip().split(',')):
+                count += 1
+
+    print(count)
